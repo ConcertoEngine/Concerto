ce6deec0ebf35d08ad121d5e2792fbf271790f99
Arthur Vasseur <arthur.vasseur@epitech.eu>
2022-05-22 11:18:57 +0200

    refactor: remplacement of the Entity class by an Id

--- include/ecs/Entity.hpp
+++ include/ecs/Entity.hpp
@@ -13,59 +13,9 @@
 #include <stdexcept>
 #include "Component.hpp"
 
-namespace Concerto::Ecs
+namespace Concerto::Ecs::Entity
 {
-	class Entity
-	{
-	public:
-		using Id = std::size_t;
-
-		Entity() = default;
-
-		Entity(Entity&&) = default;
-
-		Entity(const Entity&) = delete;
-
-		Entity& operator=(Entity&&) = delete;
-
-		Entity& operator=(const Entity&) = delete;
-
-		template<typename Comp, typename... Args>
-		Comp& emplaceComponent(Args&& ...args)
-		{
-			Component::Id id = Component::getId<Comp>();
-			auto it = _components.find(id);
-			if (it != _components.end())
-				throw std::runtime_error("Component already exists");
-			auto any = std::make_any<Comp>(std::forward<Args>(args)...);
-			auto newElement = _components.emplace(id, any);
-			_instanciatedComponents[id] = true;
-			return std::any_cast<Comp&>(std::get<0>(newElement)->second);
-		}
-
-		template<typename Comp>
-		void removeComponent()
-		{
-			Component::Id id = Component::getId<Comp>();
-			auto it = _components.find(id);
-			if (it == _components.end())
-				throw std::runtime_error("Component does not exist");
-			_instanciatedComponents[id] = false;
-			_components.erase(it);
-		}
-
-		template<typename Comp>
-		bool hasComponent() const
-		{
-			Component::Id id = Component::getId<Comp>();
-			return _instanciatedComponents[id];
-		}
-
-	private:
-		std::bitset<64> _instanciatedComponents{};
-		std::unordered_map<Component::Id, std::any> _components{};
-	};
-
+	using Id = std::size_t;
 }
 
 #endif //CONCERTO_ENTITY_HPP
