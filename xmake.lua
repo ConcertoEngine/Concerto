1412d909d95d12f24ea8943564d2bbf645f8d986
Arthur Vasseur <arthur.vasseur@epitech.eu>
2023-03-02 17:10:41 +0900

    feat: sponza scene auto download

--- xmake.lua
+++ xmake.lua
@@ -2,7 +2,7 @@ add_rules('mode.debug')
 add_repositories('Concerto-xrepo https://github.com/ConcertoEngine/xmake-repo.git main')
 add_repositories('nazara-engine-repo https://github.com/NazaraEngine/xmake-repo.git')
 add_requires('gtest', 'ConcertoCore')
-add_requires('nazaraengine', { configs = { audio = false, network = false, physics2d = false, physics3d = false, utility = true, plugin_assimp = false, with_symbols = true  } })
+add_requires('nazaraengine', { configs = { audio = false, network = false, physics2d = false, physics3d = false, utility = true, plugin_assimp = true, with_symbols = true  } })
 
 target('Concerto')
     set_kind('binary')
@@ -14,6 +14,22 @@ target('Concerto')
     add_packages('nazaraengine')
     add_packages('ConcertoCore')
     after_build(function(target)
+        import('net.http')
+        import('utils.archive')
+
+        if (not os.exists('assets/sponza')) then
+            print('Downloading assets...')
+            http.download('https://casual-effects.com/g3d/data10/common/model/crytek_sponza/sponza.zip', 'assets/sponza.zip')
+            print('Downloading assets... Done !')
+
+            print('Extracting assets...')
+                archive.extract('assets/sponza.zip', 'assets/sponza')
+                os.rm('assets/sponza.zip')
+            print('Extracting assets... Done !')
+        else
+            print('Assets already downloaded !')
+        end
+
         print("Copying resources...")
         local binaryPath = "$(buildir)/$(plat)/$(arch)/$(mode)"
         os.mkdir(binaryPath .. "/assets")
