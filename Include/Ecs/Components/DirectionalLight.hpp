7ad7de8dee37e24a49ba4ea5863c2a5d1e793bec
Arthur Vasseur <arthur.vasseur@epitech.eu>
2023-03-02 17:10:07 +0900

    feat: add a directional light component

---
+++ Include/Ecs/Components/DirectionalLight.hpp
@@ -0,0 +1,22 @@
+//
+// Created by arthur on 02/03/2023.
+//
+
+#ifndef CONCERTO_INCLUDE_ECS_COMPONENTS_DIRECTIONALLIGHT_HPP_
+#define CONCERTO_INCLUDE_ECS_COMPONENTS_DIRECTIONALLIGHT_HPP_
+
+#include "Concerto/Core/Math/Quaternion.hpp"
+#include "Concerto/Core/Math/Vector.hpp"
+#include "Color.hpp"
+
+namespace Concerto
+{
+	struct DirectionalLight
+	{
+		Color color;
+		Math::Quaternionf rotation;
+		Math::Vector3f direction;
+	};
+}
+
+#endif //CONCERTO_INCLUDE_ECS_COMPONENTS_DIRECTIONALLIGHT_HPP_
