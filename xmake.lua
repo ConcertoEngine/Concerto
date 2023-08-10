262a8e450ab6eb43a560bf91209418d0a8c58ea7
Arthur Vasseur <arthur.vasseur@epitech.eu>
2023-08-10 11:10:39 +0200

    feat: compiling Concerto in static lib

--- xmake.lua
+++ xmake.lua
@@ -1,41 +1,17 @@
 add_rules('mode.debug')
 add_repositories('Concerto-xrepo https://github.com/ConcertoEngine/xmake-repo.git main')
+
 add_requires('gtest', 'ConcertoCore', 'ConcertoGraphics')
+add_requireconfs("**.vulkan-headers", {override = true, version = "1.3.246"})
 
 target('Concerto')
---     set_runtimes(is_mode('debug') and 'MDd' or 'MD')
-    set_kind('binary')
+    set_kind('static')
     set_symbols('debug')
-    set_warnings('everything')
-    set_optimize('none')
+    set_warnings('allextra')
     set_languages('cxx20')
-    add_includedirs('Include', 'Include/Ecs', 'Include/Ecs/Systems', 'Include/Ecs/Systems/NazaraRenderer', 'Include/Ecs/Components')
+    add_includedirs('Include', 'Include/Ecs', 'Include/Ecs/Systems', 'Include/Ecs/Components')
     add_files('Src/**.cpp')
     add_packages('ConcertoCore', 'ConcertoGraphics')
-    after_build(function(target)
-        import('net.http')
-        import('utils.archive')
-
-        if (not os.exists('assets/sponza')) then
-            print('Downloading assets...')
-            http.download('https://casual-effects.com/g3d/data10/common/model/crytek_sponza/sponza.zip', 'assets/sponza.zip')
-            print('Downloading assets... Done !')
-
-            print('Extracting assets...')
-                archive.extract('assets/sponza.zip', 'assets/sponza')
-                os.rm('assets/sponza.zip')
-            print('Extracting assets... Done !')
-        else
-            print('Assets already downloaded !')
-        end
-
-        print("Copying resources...")
-        local binaryPath = "$(buildir)/$(plat)/$(arch)/$(mode)"
-        os.mkdir(binaryPath .. "/assets")
-        os.cp("./config.json", binaryPath .. "/config.json")
-        os.cp("assets/**", binaryPath .. "/assets")
-        print("Copying resources... Done !")
-    end)
 
 target('ConcertoUnitTests')
     set_kind('binary')
@@ -44,5 +20,6 @@ target('ConcertoUnitTests')
     set_languages('cxx20')
     set_optimize('none')
     add_includedirs('Include', 'Include/Ecs', 'Include/Ecs/Systems')
-    add_files('Src/Ecs/*.cpp', 'tests/*.cpp')
+    add_files('tests/*.cpp')
     add_packages('gtest', 'ConcertoCore')
+    add_deps('Concerto')
