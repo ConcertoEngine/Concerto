a5b0918bb00da41a7404c0d591a8648b95745ccd
Arthur Vasseur <arthur.vasseur@epitech.eu>
2023-08-10 11:25:25 +0200

    feat: xmake add header files

--- xmake.lua
+++ xmake.lua
@@ -10,6 +10,8 @@ target('Concerto')
     set_warnings('allextra')
     set_languages('cxx20')
     add_includedirs('Include', 'Include/Ecs', 'Include/Ecs/Systems', 'Include/Ecs/Components')
+    add_headerfiles('Include/(Concerto/*.hpp)','Include/(Concerto/Ecs/*.hpp)', 'Include/(Concerto/Ecs/Components/*.hpp)', 'Include/(Concerto/Ecs/Systems/*.hpp)')
+    add_headerfiles('Include/(Concerto/*.inl)','Include/(Concerto/Ecs/*.inl)', 'Include/(Concerto/Ecs/Components/*.inl)', 'Include/(Concerto/Ecs/Systems/*.inl)')
     add_files('Src/**.cpp')
     add_packages('ConcertoCore', 'ConcertoGraphics')
 
