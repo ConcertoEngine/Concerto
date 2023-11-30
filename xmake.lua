9af64464098a4a99152cccfa83a36e5768c9a768
Arthur Vasseur <arthur.vasseur@epitech.eu>
2023-11-30 16:33:14 +0100

    feat: add the possibility to not use the graphics module

--- xmake.lua
+++ xmake.lua
@@ -2,14 +2,19 @@ add_rules("mode.debug", "mode.release")
 add_rules("plugin.vsxmake.autoupdate")
 
 add_repositories('Concerto-xrepo https://github.com/ConcertoEngine/xmake-repo.git main')
-add_repositories("nazara-repo https://github.com/NazaraEngine/xmake-repo.git")
 
 add_requires('gtest')
-add_requires("nazaraengine", {configs = { debug = is_mode("debug"), with_symbols = true, audio = false, bulletphysics3d = false, chipmunkphysics2d = false, graphics = true, joltphysics3d = false, platform = true, renderer = true, utility = true, widgets = false, plugin_assimp = false, network = false }})
 add_requires('ConcertoCore', {configs = { debug = is_mode("debug"), with_symbols = true}})
 
 if is_plat("windows") then
     set_runtimes(is_mode("debug") and "MDd" or "MD")
+end
+
+option("graphics", { description = "Enable graphics module", default = true})
+
+if has_config("graphics") then
+    add_repositories("nazara-repo https://github.com/NazaraEngine/xmake-repo.git")
+    add_requires("nazaraengine", {configs = { debug = is_mode("debug"), with_symbols = true, audio = false, bulletphysics3d = false, chipmunkphysics2d = false, graphics = true, joltphysics3d = false, platform = true, renderer = true, utility = true, widgets = false, plugin_assimp = false, network = false }})
 end
 
 target('ConcertoEngine')
@@ -28,7 +33,9 @@ target('ConcertoEngine')
     add_files('Src/**.cpp')
     add_defines("CONCERTO_ENGINE_BUILD")
     add_packages('ConcertoCore', {public = true})
-    add_packages('nazaraengine', {public = true})
+    if has_config("graphics") then
+        add_packages('nazaraengine', {public = true})
+    end
 
 target('ConcertoEngineUnitTests')
     set_kind('binary')
